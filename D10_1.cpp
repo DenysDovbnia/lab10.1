0da3414dbe78492d8fef83f44f298d4695b33575
Denys Dovbnya <76188167+Denys-Dovbnya@users.noreply.github.com>
2023-05-08 08:43:55 +0300

    Create D10_1.cpp

---
+++ D10_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool check_triples(const string& filename) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        for (int i = 0; i < line.size() - 2; i++) {
+            if (line.substr(i, 3) == "OGO" || line.substr(i, 3) == "AGA") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    if (check_triples("t.txt")) {
+        cout << "Found OGO or AGA" << endl;
+    }
+    else {
+        cout << "Did not find OGO or AGA" << endl;
+    }
+    return 0;
+}
